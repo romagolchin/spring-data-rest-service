da4edae9244f301b8659339babece2dc0d67f65f
romagolchin <gol4in.roman@yandex.ru>
2018-06-26 15:50:18 +0300

    make small fixes

--- src/main/java/service/Application.java
+++ src/main/java/service/Application.java
@@ -1,38 +1,53 @@
 package service;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import lombok.*;
 
 import javax.persistence.*;
 import java.time.LocalDate;
 
 @Entity
-@NoArgsConstructor
-@ToString
 public class Application {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     @JsonProperty("APPLICATION_ID")
-    @Getter
     private long id;
 
     @JsonProperty("CONTACT_ID")
-    @Getter
+    
     private long contactId;
 
     @Column(nullable = false)
     @JsonProperty("DT_CREATED")
-    @Getter
+    
     private LocalDate dateCreated;
 
     @Column(nullable = false)
     @JsonProperty("PRODUCT_NAME")
-    @Getter
+    
     private String productName;
 
     public Application(long contactId, LocalDate dateCreated, String productName) {
         this.contactId = contactId;
         this.dateCreated = dateCreated;
         this.productName = productName;
+    }
+
+    public Application() {
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public long getContactId() {
+        return contactId;
+    }
+
+    public LocalDate getDateCreated() {
+        return dateCreated;
+    }
+
+    public String getProductName() {
+        return productName;
     }
 }
